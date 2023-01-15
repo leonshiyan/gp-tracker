0a8c2b3702c2cfed6e0ac9f3f4e90e076a9f45d0
leonshiyan <leonshiyan@gmail.com>
2023-01-15 11:31:49 -0800

    add station router in server

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ import'./config/passport.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as transacRounter } from './routes/transaction.js'
+import { router as stationRounter } from './routes/station.js'
+
 // create the express app
 const app = express()
 
@@ -66,7 +68,7 @@ app.use(passDataToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/transactions', transacRounter)
-
+app.use('/stations', stationRounter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
