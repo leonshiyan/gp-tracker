4edb361ec98ef48d83fa34919cc68519e26e1b5a
leonshiyan <leonshiyan@gmail.com>
2023-01-18 11:51:38 -0800

    update profile view

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,17 +1,23 @@
 import { Profile } from '../models/profile.js'
+import { Transaction } from '../models/transaction.js'
 
 function index(req, res) {
   Profile.find({})
   .then(profiles => {
-    res.render('profiles/index', {
-      profiles,
-			title: "Your Profile"
+    Profile.findById(req.user.profile._id)
+    .populate('transactions')
+    .then(myProfile =>{
+      res.render('profiles/index', {
+        profiles,
+        myProfile,
+        title: "Your Profile"
     })
   })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/')
-  })
+})
+.catch(err => {
+  console.log(err)
+  res.redirect('/')
+})
 }
 
 export {
